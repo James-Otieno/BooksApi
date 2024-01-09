46c3a66b24e9c9d54d424b1d76ab68cda97423a9
james-otieno <jemo9434@gmail.com>
2024-01-10 01:08:48 +0300

    updated constructor and Book static method

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,14 +7,28 @@ namespace BooksApi.Models
         public Guid BookID { get; set; }
         public string BookName { get; set; }
         public string BookAuthor { get; set; }
+        public string BookGenre { get; set; }
+        public string publisher { get; set; }
+        public DateTime YearOfPublication { get; set; }
 
-        public Book(string bookName, string bookAuthor)
+        public Book(string bookName, string bookAuthor, string bookGenre, string publisher, DateTime yearOfPublication)
         {
             BookName = string.IsNullOrWhiteSpace(bookName) ? bookName : "null";
             BookAuthor = string.IsNullOrWhiteSpace(bookAuthor) ? bookAuthor:"null";
-            
-           
+            BookGenre = string.IsNullOrWhiteSpace(bookGenre) ? bookGenre:"null";
+            publisher = string.IsNullOrWhiteSpace(publisher) ? publisher:"null";
+            YearOfPublication = yearOfPublication;
 
+
+
+
+
+
+        }
+
+        public static Book AddNewBook(string bookName, string bookAuthor, string bookGenre, string publisher, DateTime yearOfPublication) { 
+
+            return new Book(bookName, bookAuthor, bookGenre, publisher, yearOfPublication);
         }
     }
 }
